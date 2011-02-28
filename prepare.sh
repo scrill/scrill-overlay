eed1bae39f25548ddb920ebc0117c72e544c362f
Scrill <scrill@ya.ru>
2011-02-28 08:41:35 -0500

    fucking github formating. back 2 the roots

--- prepare.sh
+++ prepare.sh
@@ -1,36 +1,32 @@
 #!/usr/bin/env bash
 
-cat > ./README.md << "EOF"
+cat > ./README << "EOF"
+### My Gentoo overlay ###
 
-# Description
+Configuration:
 
-My Gentoo overlay.
+1. Add 'https://github.com/Scrill/scrill-overlay/raw/master/scrill-overlay.xml' to layman.cfg in 'overlays' section.
+2. Enable overlay in layman:
+  # layman -f
+  # layman -a scrill
 
-# Configuration
-
-* Add 'https://github.com/Scrill/scrill-overlay/raw/master/scrill-overlay.xml' to layman.cfg in 'overlays' section.
-* Enable overlay in layman:
-
-    layman -f
-    layman -a scrill
-
-# Content
+Content:
 
 EOF
 
 for category in `ls -1 | grep -e "...-.*"`; do
     if [ -d "./${category}" ]; then
-	echo -e "* ${category}\n" >> ./README.md
+	echo "# ${category}" >> ./README
 	for package in `ls -1 ./${category}`; do
-	    echo -ne "    Package: ${package}\n    Versions: " >> ./README.md
+	    echo -ne "\tPackage:\t${package}\n\tVersions:\t" >> ./README
 	    for ebuild in `ls -1 ./${category}/${package} | grep -e "ebuild$"`; do
-		echo -n "$ebuild" | sed -E s%${package}-\|\.ebuild%%g >> ./README.md
-		echo -n "  " >> ./README.md
+		echo -n "$ebuild" | sed -E s%${package}-\|\.ebuild%%g >> ./README
+		echo -n "  " >> ./README
 	    done
 	    ebuild ./${category}/${package}/${ebuild} manifest
-	    echo -ne "\n    Description: " >> ./README.md
-	    cat ./${category}/${package}/${ebuild} | grep 'DESCRIPTION' | awk -F '"' '{print $2}' >> ./README.md
-	    echo >> ./README.md
+	    echo -ne "\n\tDescription:\t" >> ./README
+	    cat ./${category}/${package}/${ebuild} | grep 'DESCRIPTION' | awk -F '"' '{print $2}' >> ./README
+	    echo >> ./README
 	done
     fi
 done
